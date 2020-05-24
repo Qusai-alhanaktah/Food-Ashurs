c7e359d1cef3dc39b50cb7810d8892d56a2f2f5a
Qusai-alhanaktah <qusaialhanaktah@gmail.com>
2020-05-24 23:29:01 +0300

    fix profile page

--- component/profile.js
+++ component/profile.js
@@ -1,14 +1,29 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { StyleSheet, Text, View, TextInput, Button, Modal, Image, TouchableOpacity } from 'react-native';
+import { AsyncStorage } from 'react-native';
 
 export default function Profile(props) {
-    return (
-        <View style={styles.container}>
-          <Text>Hello {props.route.params.username}</Text>
-          <Text>You enter as: {props.route.params.role}</Text>  
-          <Text>Your Email is: {props.route.params.email}</Text>  
+  const [user, setUser] = useState({});
+  const [showUser, setShowUser] = useState(false);
 
+  useEffect(()=>{
+    AsyncStorage.getItem('user').then(userInfo => {
+      console.log('userInfo', typeOf(userInfo));
+      setUser(JSON.parse(userInfo));
+      setShowUser(true)
+    })
+  });
+  
+    return (
+        <>
+        {showUser && (
+        <View style={styles.container}>
+          <Text>Hello {user.username}</Text>
+          <Text>You enter as: {user.role}</Text>  
+          <Text>Your Email is: {user.email}</Text>  
         </View>
+        )}
+        </>
     )
 }
 const styles = StyleSheet.create({
